bb93ae4b9ea714578da044d82494e5848ea9198f
thejackshelton <me@jackshelton.com>
2024-01-11 11:17:05 -0600

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import MyCombobox from "~/components/combobox/combobox"
 
 export default component$(() => {
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <MyCombobox />
   );
 });
 
